1da4a00bb6b368a80fb6729fe96d12a52986c1f0
Detective Pikachu <frenzyexists@gmail.com>
2023-08-10 21:39:45 -0400

    Mayor fix at Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import routes from '~pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+import generatedRoutes from 'virtual:generated-pages'
 
-const markdownRoutes = routes.map(v => ({...v, visibleToNav: false}))
+const routes2 = setupLayouts(generatedRoutes)
+const marky = routes2.map(v => ({ ...v, visibleToNav: false }))
 
 const routeArray = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
+    component: () => import('../views/HomeView.vue')
+  }, {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/AboutView.vue')
-  },
-  {
+  }, {
     path: '/projects',
     name: 'projects',
     component: () => import('../views/ProjectView.vue')
-  },
-  {
+  }, {
     path: '/blog',
     name: 'blog',
     component: () => import('../views/BlogView.vue')
-  },
-  {
+  }, {
     path: "/projects/:letter",
     name: "projectByLetter",
     component: () => import('../views/ByListProjectView.vue'),
     visibleToNav: false
-  },
-  {
+  }, {
     path: "/blog/:letter",
     name: "blogByLetter",
     component: () => import('../views/ByListBlogView.vue'),
     visibleToNav: false
-  },
-  {
+  }, {
     path: "/:pathMatch(.*)",
     name: "404",
     component: () => import('../views/404View.vue'),
     visibleToNav: false
   }
-].concat(markdownRoutes)
+].concat(marky)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routeArray
+  routes: routeArray,
+  scrollBehavior(to, from, savePosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+  }
 })
 
-export { routeArray, markdownRoutes }
+export { routeArray, marky }
 export default router
