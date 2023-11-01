f98d26b8d83d487e37f7a2a5718e80284bb934eb
Detective Pikachu <frenzyexists@gmail.com>
2023-11-01 05:52:22 -0400

    Updating build

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <div class="flex flex-col h-screen main-app md:pt-16">
-    <header></header>
-    <RouterView v-slot="{ Component }">
-      <component :is="Component" :key="$route.path"></component>
-    </RouterView>
-  </div>
+	<router-view />
 </template>
