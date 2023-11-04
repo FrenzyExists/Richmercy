5df4d5b00537298f6128fe6b806770589332cbaa
Detective Pikachu <frenzyexists@gmail.com>
2023-11-04 14:15:23 -0400

    about added

---
+++ src/pages/about.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="pt-8 md:pt-10">
+        <div class="absolute bg-color-about-me inset-0"></div>
+        <div
+            class="md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16">
+            <div class="prose prose-toy-story dark:prose-invert font-montserrat-alternate mx-auto">
+                <h1>Who Am I?</h1>
+                <p>
+                    Hi! My name is Angel, but is very likely you know me as
+                    <span class="italic">Pikachu</span> I'm a Computer Engineering student at the University
+                    of Puerto Rico in Mayaguez. I've always been fascinated by the way computers and circuits
+                    work and how they can be used to solve complex problems. My passion for programming
+                    started since my days in middle school when playing Flash games like Smash Flash 2.
+                    Youtube had lots of tutorials on Game Maker and when I got in highschool I started to
+                    "program" with tutorials. Although I was able to make a simple platform game, I still had
+                    trouble understanding object-oriented concepts like polyphormism and posponed my hobbie.
+                    Several years later I would take the basic courses like Algorithms and Advanced
+                    Programming to finally set myself on switching from studying Surveying Engineering to
+                    Computer Engineering on my 4th year. And although the change wasn't easy, I don't regret
+                    it!
+                </p>
+
+                <p>
+                    I am currently working on a research project with a professor in the Electrical
+                    Engineering department where we are developing a GUI for an existing testing CLI developed
+                    by Texas Instruments engineers.
+                </p>
+
+                <p>
+                    I also currently develop frontend in my free time and experiment with Arduino and ESP-32
+                    boards. I am also an active member of the IEEE Circuits and Systems Asociation as a
+                    Webmaster asistant and as Webmaster for the IEEE in my university. I am excited about the
+                    future of technology and how it can be used to solve real-world problems. I hope to be
+                    part of this change by contributing my skills and knowledge.
+                </p>
+                <br />
+                <div class="text-center mx-auto max-w-4xl px-3">
+                    <h2>Let's work together</h2>
+                    <p>
+                        Whether you're interested in working with me or just want to say hello, I'd love to hear
+                        from you!
+                    </p>
+                    
+                    <br />
+                </div>
+            </div>
+        </div>
+    </div>
+    <foot />
+</template>
+  
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@ useHead({
 
 
 <template>
-	<navbar/>
+	<navbar />
 	<div class="w-full m-auto dark:bg-dark-background bg-background">
 		<div class="xl:max-w-7xl lg:max-w-5xl lg:p-6 mld:max-w-3xl mld:p-4 m-auto xs:w-full xs:p-4">
 			<div class="xl:py-20 lg:py-28 mld:py-10 xs:py-10">
@@ -26,7 +26,25 @@ useHead({
 			</div>
 		</div>
 	</div>
-	<foot/>
+
+	<div>
+		<div class="text-center xl:py-20 lg:py-20 mld:py-10 xs:py-10">
+			<div class="text-center m-auto max-w-4xl">
+				<h2
+					class="font-semibold italic dark:text-dark-bg-super-soft-trans text-bg-super-soft-trans lg:text-5xl mld:text-5xl xs:text-4xl text-3xl after:absolute after:bottom-0 after:left-0 after:right-0 after:m-auto dark:after:bg-dark-acc after:bg-acc after:h-1 after:w-32 relative mb-16 pb-9">
+					Building the future one line of code at a time
+				</h2>
+				<p class="sm:text-lg lg:text-xl text-text dark:text-dark-text">
+					As a full stack developer, I have experience working with both front-end and back-end
+					technologies. I'm also passionate about tinkering with hardware and exploring new
+					technologies. Whether you're looking to build a new website or develop an innovative
+					hardware solution, I'm here to help you bring your ideas to life.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<foot />
 </template>
 
 <route lang="yaml">
